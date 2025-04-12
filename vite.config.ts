9a6820710217fb668f3cd4b6c940cae9a2f5cdac
jared <jaredkjar@gmail.com>
2025-04-12 15:03:52 -0600

    github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/wanderoot/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
+    emptyOutDir: true,
+    sourcemap: true
   }
 })
